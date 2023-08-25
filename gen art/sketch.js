9a36d65c968637934b6d645f03ba28cbd65a1ce1
CORDIUMcode <ccharkin@gmail.com>
2023-08-24 21:22:02 -0400

    commit

--- gen art/sketch.js
+++ gen art/sketch.js
@@ -1,42 +1,53 @@
-const CRYSTAL_SIZE = 500;
-const SIDES = 6;
-let PALETTE = [];
+const CRYSTAL_SIZE = 500
+const SIDES = 6
+let PALETTE = []
+
+const layers = []
+
 
 function setup() {
-  createCanvas(530, 530, SVG);
+  createCanvas(530, 530, SVG)
 
   PALETTE = [
     color(255, 52, 154), // pink
     color(4, 0, 152)    // blue
-  ];
+  ]
 
   noLoop();
-  angleMode(DEGREES);
-  rectMode(CENTER);
-
-  // Add a mouse click event listener to the canvas
-  canvas.mouseClicked(restartDraw);
+  angleMode(DEGREES)
+  rectMode(CENTER)
+  
 }
 
 function draw() {
-  background(255); // Clear the background
 
-  const circles = new Circles();
-  circles.render();
+  // const circles = new Circles()
+  // circles.render();
 
-  const simpleLines = new SimpleLines();
-  simpleLines.render();
+  // const simpleLines = new SimpleLines()
+  // simpleLines.render();
 
-  const outlineShape = new OutlineShape();
-  outlineShape.render();
+  // const outlineShape = new OutlineShape()
+  // outlineShape.render()
 
-  //testLines();
+  //testLines()
+  //outlineShape()
+  // simpleLines()
+  //circles()
+
+  let picker = random(1)
+  if (picker > 0.3) {
+    layers.push(new OutlineShape())
+  }
+
+  picker = random(1)
+  if (picker > 0.3) {
+    layers.push(new SimpleLines())
+  }
+
+  picker = random(1)
+  if (picker > 0.3) {
+    layers.push(new Circles())
+  }
 }
 
-// Call the draw function every 2 seconds (2000 milliseconds)
-setInterval(draw, 500);
-
-// Function to restart the draw loop on mouse click
-function restartDraw() {
-  loop(); // Start the loop
-}
